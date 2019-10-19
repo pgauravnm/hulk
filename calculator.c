da65cae7b6ad11b23e760b75f87a8805e634dc6d
P.GAURAV.N.M <gaurav.6302665991.n.m.p@gmail.com>
2019-10-19 19:28:05 +0530

    diff

--- calculator.c
+++ calculator.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
-
+int diff(int a,int b)
+{
+return b-a;
+}
+int sum(int a,int b){
+return a-b;
+}
 int main()
 {
 int a=5 , b=10;
-int c;
+int c,d;
 
-c=sum(a,b);
-
-printf("sum=%d",c);
-printf("diff=%d",b-a);
+c=diff(a,b);
+d=sum(a,b);
+printf("diff=%d",c);
+printf("sum=%d",d);
 return 0;
 }
